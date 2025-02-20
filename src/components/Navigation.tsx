8369173ddd6244034e4174c5064ab4bb25c889a4
Luis Arguijo <rodriguez.arguijo@gmail.com>
2025-02-20 16:01:48 +0100

    Mobile navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,22 +1,89 @@
-import { Navbar, Nav } from "rsuite";
+import { useState, useEffect } from "react";
+import { IconButton, Drawer, Navbar, Nav, Stack, useMediaQuery } from "rsuite";
+import { FaBars } from "react-icons/fa";
 import styles from "@/styles/Navigation.module.css";
 
-const navigationOptions = [
+type NavigationOption = {
+  href: string;
+  label: string;
+};
+
+const navigationOptions: NavigationOption[] = [
   { label: "Nuestra Iglesia", href: "/acerca-de" },
   { label: "Ministerios", href: "/ministerios" },
 ];
-const Navigation = () => {
-  return (
-    <Navbar className={styles.navigation}>
-      <Nav>
-        {navigationOptions.map(({ label, href }) => (
-          <Nav.Item key={href} className={styles.navigationItem} href={href}>
-            {label}
+
+type NavigationOptionsProps = {
+  options: NavigationOption[];
+};
+
+const MobileNavigation = ({ options }: NavigationOptionsProps) => {
+  const [open, setOpen] = useState<boolean>();
+
+  if (!open) {
+    return (
+      <Navbar className={`${styles.navigation} ${styles.mobile}`}>
+        <Nav>
+          <Nav.Item
+            className={styles.navigationItem}
+            onClick={() => setOpen(true)}
+          >
+            <IconButton circle icon={<FaBars />} appearance="link" size="lg" />
           </Nav.Item>
-        ))}
-      </Nav>
-    </Navbar>
+        </Nav>
+      </Navbar>
+    );
+  }
+  return (
+    <Drawer
+      placement="top"
+      open={open}
+      onClose={() => setOpen(false)}
+      closeButton
+    >
+      <Drawer.Body>
+        <Navbar className={`${styles.navigation}`}>
+          <Nav>
+            <Stack direction="column">
+              {options.map(({ label, href }) => (
+                <Nav.Item
+                  key={href}
+                  className={styles.navigationItem}
+                  href={href}
+                >
+                  {label}
+                </Nav.Item>
+              ))}
+            </Stack>
+          </Nav>
+        </Navbar>
+      </Drawer.Body>
+    </Drawer>
   );
+};
+
+const DesktopNavigation = ({ options }: NavigationOptionsProps) => (
+  <Navbar className={`${styles.navigation}`}>
+    <Nav>
+      {options.map(({ label, href }) => (
+        <Nav.Item key={href} className={styles.navigationItem} href={href}>
+          {label}
+        </Nav.Item>
+      ))}
+    </Nav>
+  </Navbar>
+);
+
+const Navigation = () => {
+  const [isMobile] = useMediaQuery("(max-width: 700px)");
+  const [isClient, setIsClient] = useState<boolean>(false);
+  const NavigationWrapper =
+    isMobile && isClient ? MobileNavigation : DesktopNavigation;
+
+  useEffect(() => {
+    setIsClient(true); // Ensures rendering only happens on the client
+  }, []);
+  return <NavigationWrapper options={navigationOptions} />;
 };
 
 export default Navigation;
